07f66ac4baee92c3523412bc966c7449122a2082
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2026-02-27 14:21:11 -0600

    lesson 3

---
+++ lesson.js
@@ -0,0 +1,5 @@
+let str = 'The grass is green';
+
+let newStr = str.slice(4, 9);
+
+console.log(newStr);
---
+++ lesson3.js
@@ -0,0 +1,28 @@
+function displayBoard(board) {
+  console.log('');
+  console.log('     |     |');
+  console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
+  console.log('     |     |');
+  console.log('-----+-----+-----');
+  console.log('     |     |');
+  console.log(`  ${board['4']}  |  ${board['5']}  |  ${board['6']}`);
+  console.log('     |     |');
+  console.log('-----+-----+-----');
+  console.log('     |     |');
+  console.log(`  ${board['7']}  |  ${board['8']}  |  ${board['9']}`);
+  console.log('     |     |');
+  console.log('');
+}
+
+function initializeBoard() {
+  let board = {};                           // start with empty object
+  for (let square = 1; square <= 9; square++) {  // loop 1 through 9
+    board[String(square)] = ' ';           // add each square as a key, value is empty space
+  }
+  return board;                            // return the completed board
+}
+
+let board = initializeBoard();
+
+
+displayBoard(board);
